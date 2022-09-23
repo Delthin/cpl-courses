da00780ccbbd207463557a16cf1157817cb48cc8
Delthin <1059661071@qq.com>
2022-09-23 15:30:01 +0800

    sb

--- hello.c
+++ hello.c
@@ -1,7 +1,18 @@
+//单行注释
+/*多行注释
+
+
+*/
 #include <stdio.h>
 
 int main()
+// main fuction
 {
     printf("Hello world!\n");
+    /*\n newline
+    printf print+format
+    */
     return 0;
+    // return statement
 }
+// gcc hello.c -o hello
