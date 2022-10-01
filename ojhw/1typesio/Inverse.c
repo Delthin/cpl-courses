38aa74da911843e479fbdae2ad32d2fbd9bbb57a
Delthin <1059661071@qq.com>
2022-10-01 17:36:07 +0800

    haha

--- ojhw/1typesio/Inverse.c
+++ ojhw/1typesio/Inverse.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 int main()
 {
-    char
-        scanf("%d", &num);
+    int num, a, b, c, d, e;
+    scanf("%d", &num);
+    a = num / 10000;
+    num -= (a * 10000);
+    b = num / 1000;
+    num -= (b * 1000);
+    c = num / 100;
+    num -= (c * 100);
+    d = num / 10;
+    e = num - d * 10;
+
+    printf("%d%d%d%d%d", c, b, a, e, d);
 
     return 0;
 }
