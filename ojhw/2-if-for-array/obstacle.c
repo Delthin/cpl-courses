0edbaee06c8bddd606053cdede3b4bd2e5ba2ee4
Delthin <1059661071@qq.com>
2022-10-14 13:56:50 +0800

    sb

--- ojhw/2-if-for-array/obstacle.c
+++ ojhw/2-if-for-array/obstacle.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
 #include <math.h>
 int main(){
-    int n = 0;
-    char track[10] = {0};
-    int xa = 0,xb = 0,xc = 0, ya = 0, yb = 0, yc = 0;
-    scanf("%d%d%d%d%d%d",&xa,&ya,&xb,&yb,&xc,&yc);
-    int x = xa,y= ya;
+    long long int n = 0;
+    char track[10000000] = {0};
+    long long int xa = 0,xb = 0,xc = 0, ya = 0, yb = 0, yc = 0;
+    scanf("%lld%lld%lld%lld%lld%lld",&xa,&ya,&xb,&yb,&xc,&yc);
+    long long int x = xa,y= ya;
 
+    if (xa == xb && xb == xc){
+        if ((ya < yc && yc < yb) || (yb < yc && yc < ya)){x++; track[n] = 'R'; n++; goto st2;}
+        else {goto st2;}}
+    else if (ya == yb && yb == yc){
+        if ((xa < xc && xc < xb) || (xb < xc && xc < xa)){y++; track[n] = 'U'; n++; goto st1;}
+        else {goto st1;}}
+    else if (xa == xc || yb == yc){goto st1;}
+    else if (ya == yc || xb == xc){goto st2;}
 st1: 
-    if(abs(x - xc) == 1){goto st2;}    
-    else if (x < xb){x++; n++; track[n] = 'R'; goto st1;}
-    else if (x > xb){x--; n++; track[n] = 'L'; goto st1;}
+    if (x < xb){x++; track[n] = 'R'; n++; goto st1;}
+    else if (x > xb){x--; track[n] = 'L'; n++;goto st1;}
     else if (x == xb && y == yb){goto st3;}
     else {goto st2;}
 st2:
-    if(abs(y - yc) == 1){goto st1;}    
-    else if (y < yb){y++; n++; track[n] = 'U'; goto st2;}
-    else if (y > yb){y--; n++; track[n] = 'D'; goto st2;}
+    if (y < yb){y++; track[n] = 'U'; n++;goto st2;}
+    else if (y > yb){y--; track[n] = 'D'; n++;goto st2;}
     else if (x == xb && y == yb){goto st3;}
     else {goto st1;}
 st3:
     printf("%d\n%s",n,track);
-
     return 0;
 }
